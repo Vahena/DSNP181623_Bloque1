456c50b46e29c14cc2a1194d44e1910b8607cc5f
SIRIA NATHALIE HERNANDEZ VALENCIA <DSNP181623@ugb.edu.sv>
2023-09-23 12:35:44 -0400

    Add project files.

---
+++ DSNP181623_Bloque1/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+        While (True)
+            Try
+                Dim edad As Byte
+
+                Console.WriteLine("Ingrese su edad:")
+                edad = Console.ReadLine
+
+                Select Case edad
+                    Case 0 To 17
+                        Console.WriteLine("Eres menor de edad")
+                    Case 18 To 127
+                        Console.WriteLine("Eres mayor de edad")
+                    Case Else
+                        Console.WriteLine("Deberian darte un record Guiness!")
+                End Select
+
+
+
+                Console.WriteLine("Si desea salir presione (s) si no enter:")
+                Dim salir As ConsoleKeyInfo = Console.ReadKey()
+                If salir.Key = ConsoleKey.S Then
+                    Return
+                ElseIf salir.Key = ConsoleKey.Enter Then
+                End If
+            Catch x As Exception
+                Console.WriteLine("Ingrese valores enteros")
+            End Try
+
+        End While
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
